dcbc3189656d0abe2a1670e2b3734c8e19938b38
Jennifer <jennifer15aries@gmail.con>
2024-07-23 15:20:23 -0500

    Funciona eliminar

--- js/app.js
+++ js/app.js
@@ -42,10 +42,21 @@ class AdminCitas{
         this.mostrarCita();
     }
 
+    eliminar(id){
+        this.citas = this.citas.filter( cita => cita.id !== id );
+        this.mostrarCita();
+    }
+
     mostrarCita(){
         //Limpiar Html
         while(citasCont.firstChild){
             citasCont.removeChild(citasCont.firstChild);
+        }
+
+        //Comporbar si hay citas
+        if(this.citas.length===0){
+            citasCont.innerHTML = `<p class="text-xl mt-5 mb-10 text-center">No Hay Pacientes</p>`
+            return;
         }
 
         //Generar las citas
@@ -86,6 +97,9 @@ class AdminCitas{
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
             btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+            //Funcion para eliminar
+            btnEliminar.onclick = () => this.eliminar(cita.id);
 
             const contBoton = document.createElement('DIV');
             contBoton.classList.add('flex','justify-between','mt-10');
