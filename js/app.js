97a48bcd50f2352fd1de7d8584032c9ff23773bb
Jennifer <jennifer15aries@gmail.con>
2024-07-28 14:29:00 -0500

    hello

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
 import { inputEmail, inputPaciente, inputPropietario, inputSintomas, fechaImput, formulario, citasCont, submitForm } from "./selectores.js";
 import { datosCitas,citaSubmit } from "./funciones.js";
 
+console.log('Hola')
 
 //Eventos
 inputPaciente.addEventListener('change',datosCitas);
